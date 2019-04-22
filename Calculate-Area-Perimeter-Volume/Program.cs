b390f4ce3d1c2383a82c5579e2604b107d458934
Eric Holt <fatmoti@gmail.com>
2019-04-22 14:42:03 -0400

    Setup to follow new instructions for lab

--- Calculate-Area-Perimeter-Volume/Program.cs
+++ Calculate-Area-Perimeter-Volume/Program.cs
@@ -8,14 +8,36 @@ namespace Calculate_Area_Perimeter_Volume
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //No validation required for this console app based on lab instructions
+            double length = 0.0;
+            double width = 0.0;
+            double height = 0.0;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+            while (true) //loop program until user is finished.
+            {
+                Console.WriteLine("Enter Length:");
+                length = Double.Parse(Console.ReadLine());//Get length from user. 
+                Console.WriteLine("Enter Width:");
+                width = Double.Parse(Console.ReadLine());//Get width from user.
+                Console.WriteLine("Enter Height:");
+                height = Double.Parse(Console.ReadLine());//Get height from user.
+                
+                Console.WriteLine("Area: " + (length * width));//Call CalculateArea method and display result
+
+                Console.WriteLine("Perimeter: " + (2 * (length + width)));//Call CalculatePerimeter method and display result
+
+                Console.WriteLine("Volume: " + (length * width * height));//Call CalculateVolume and display result
+
+                Console.WriteLine("Continue? (y/n):");//Does user want to preform calculations again?
+                string response = Console.ReadLine();//Get users response
+                if (response.ToLower() == "n")//Was users response n?
+                {
+                    break;//User is done and we can break out of loop.
+                }
+            }
         }
     }
 }
